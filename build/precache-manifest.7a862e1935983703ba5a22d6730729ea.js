1893b6eb0673eeed9787fe5f7b927f51fe721dda
Poornima Tanwar <tanwarpurnima05@gmail.com>
2023-12-23 12:37:02 +0530

    first commit

---
+++ build/precache-manifest.7a862e1935983703ba5a22d6730729ea.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c3c3e9a61b3c497bf9",
+    "url": "/poornima005/InterviewBook-ClientSide/static/css/main.6390f30a.chunk.css"
+  },
+  {
+    "revision": "01c3c3e9a61b3c497bf9",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/main.7c10deed.chunk.js"
+  },
+  {
+    "revision": "5fb44795d17613e9939d",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/runtime~main.a15998e0.js"
+  },
+  {
+    "revision": "21cb0c9ea3616bb73815",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/2.5d172ff2.chunk.js"
+  },
+  {
+    "revision": "7105cc0bf197fb9cb355",
+    "url": "/poornima005/InterviewBook-ClientSide/static/css/3.309204f7.chunk.css"
+  },
+  {
+    "revision": "7105cc0bf197fb9cb355",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/3.d25d941c.chunk.js"
+  },
+  {
+    "revision": "9b8ecdad322b1cdee276",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/4.7704bac6.chunk.js"
+  },
+  {
+    "revision": "fdb1c67c0165c9b54653",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/5.bc451939.chunk.js"
+  },
+  {
+    "revision": "75e417cb903749e893a4",
+    "url": "/poornima005/InterviewBook-ClientSide/static/js/6.92281941.chunk.js"
+  },
+  {
+    "revision": "0f8e789e159dcc08df61455201923173",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/defaultuser.0f8e789e.jpg"
+  },
+  {
+    "revision": "37fffae636a20102ca61bf5ff11e953a",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/1653474765011IMG_20200113_151518_101.37fffae6.jpg"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/1653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "06b4eea34c9ca56349af35e771ec592f",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/1653546797551dr-dipankar.06b4eea3.png"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/16992056566111653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/16992058093071653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/16992058156251653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/16992060400981653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "25d1d2d508e75f4a730dda26ef9b9337",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/1699866282601poornima.25d1d2d5.jpg"
+  },
+  {
+    "revision": "441546b00a1eeb910ab3928aac7904eb",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/17013265467281653475236506WhatsApp Image 2022-05-22 at 4.30.43 PM (1).441546b0.jpeg"
+  },
+  {
+    "revision": "0f8e789e159dcc08df61455201923173",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/undefined.0f8e789e.jpg"
+  },
+  {
+    "revision": "b4a4c5eb611bd473b2081d08f9d34f00",
+    "url": "/poornima005/InterviewBook-ClientSide/static/media/DAVV_logo.b4a4c5eb.png"
+  },
+  {
+    "revision": "a109d4b9805e36e2caa230af4d5c6acc",
+    "url": "/poornima005/InterviewBook-ClientSide/index.html"
+  }
+];
